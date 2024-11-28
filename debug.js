03530a496791981dc6a96f97779feb39ee712d48
CBITNTU <128826101+CBITNTU@users.noreply.github.com>
2024-11-28 13:50:17 +0000

    Add files via upload

---
+++ debug.js
@@ -0,0 +1,5 @@
+// This is a debugging utility to help identify issues in production
+console.log('Debug: Application is running');
+console.log('Debug: Window size:', window.innerWidth, 'x', window.innerHeight);
+console.log('Debug: User Agent:', navigator.userAgent);
+
