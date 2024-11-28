0c2bc7481d68d7d3c161d1661fac98c5d9baa705
CBITNTU <128826101+CBITNTU@users.noreply.github.com>
2024-11-28 11:01:28 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['framer-motion'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      use: ['babel-loader'],
+      include: [
+        /node_modules\/leaflet/,
+        /node_modules\/react-leaflet/,
+      ],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
+
