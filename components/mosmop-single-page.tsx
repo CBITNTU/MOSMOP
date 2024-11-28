9b613f7aa63f4430afe90148274a012e6cebaf60
CBITNTU <128826101+CBITNTU@users.noreply.github.com>
2024-11-28 10:58:45 +0000

    Update mosmop-single-page.tsx

--- components/mosmop-single-page.tsx
+++ components/mosmop-single-page.tsx
@@ -8,7 +8,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import Link from 'next/link'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import L from 'leaflet'
+import * as L from 'leaflet'
 import { Input } from '@/components/ui/input'
 import { Label } from "@/components/ui/label"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
