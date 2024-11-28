465d4ad0a2dcd9300aa21e839c7a76afa8d33cd9
CBITNTU <128826101+CBITNTU@users.noreply.github.com>
2024-11-28 13:49:13 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   images: {
     domains: ['cdnjs.cloudflare.com'], // Add any other domains you're loading images from
   },
+  // Add these lines for debugging
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      const originalEntry = config.entry;
+      config.entry = async () => {
+        const entries = await originalEntry();
+        if (entries['main.js'] && !entries['main.js'].includes('./src/utils/debug.js')) {
+          entries['main.js'].unshift('./src/utils/debug.js');
+        }
+        return entries;
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
